d18259aeebd0a1ecca8e01c0cb2a7da3ba42ff59
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2026-07-15 10:18:14 +0300

    Decrease peer probe timeout back

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ var (
 )
 
 const (
-	peerProbeTimeout        = 10 * time.Minute
+	peerProbeTimeout        = 10 * time.Second
 	upstreamDialTimeout     = 5 * time.Second
 	requestTimeout          = 5 * time.Minute
 	defaultLocalRPCPeerHost = "127.0.0.1"
